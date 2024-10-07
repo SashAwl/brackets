f17b906c92e8c60ffba68d789a841c01be078cb2
SashAwl <aleks.shilova@gmail.com>
2024-10-07 14:45:20 +0300

    feat: create solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) return false;
+
+  while (str.length) {
+    let inputString = str;
+    bracketsConfig.forEach(listItem => {
+      const bracketCouple = listItem.join("");
+      while (str.includes(bracketCouple)) {
+        str = str.replace(bracketCouple, "");
+      }
+    })
+    if (inputString === str) return false;
+  }
+  return true;
+  // console.log("{}}][]}[]}".replace("[]", "-"))
+
 }
+
+// function check(str, bracketsConfig) {
+//   if (str.length % 2) return false;
+
+//   while (str.length) {
+//     let inputString = str;
+//     bracketsConfig.forEach(listItem => {
+//       const bracketCouple = listItem.join("");
+//       while (str.includes(bracketCouple)) {
+//         str = str.replace(bracketCouple, "");
+//       }
+//     })
+//     if (inputString === str) return false;
+//   }
+//   return true;
+//   // console.log("{}}][]}[]}".replace("[]", "-"))
+
+// }
+
+
+// console.log(check('[]()(', [['(', ')'], ['[', ']']]))
